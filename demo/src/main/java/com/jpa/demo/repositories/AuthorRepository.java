e4ab615c1b786afaa6b37337ada863fc593f28fc
SasinduNanayakkara <it20625016@my.sliit.lk>
2025-05-28 02:27:17 +0530

    Implement Author repository

--- demo/src/main/java/com/jpa/demo/repositories/AuthorRepository.java
+++ demo/src/main/java/com/jpa/demo/repositories/AuthorRepository.java
@@ -3,5 +3,12 @@ package com.jpa.demo.repositories;
 import com.jpa.demo.models.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AuthorRepository extends JpaRepository<Author, Integer>{
+
+    List<Author> findAllByFirstName(String firstName);
+    Author findByEmail(String email);
+    List<Author> findAllByFirstNameIgnoreCase(String firstName);
+    List<Author> findAllByFirstNameContainingIgnoreCase(String firstName);
 }
