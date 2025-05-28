0e2a313061379d3b3247129feb13b06105eb0ef8
SasinduNanayakkara <it20625016@my.sliit.lk>
2025-05-29 02:45:51 +0530

    Implement new repository methods

--- demo/src/main/java/com/jpa/demo/repositories/AuthorRepository.java
+++ demo/src/main/java/com/jpa/demo/repositories/AuthorRepository.java
@@ -2,13 +2,44 @@ package com.jpa.demo.repositories;
 
 import com.jpa.demo.models.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface AuthorRepository extends JpaRepository<Author, Integer>{
 
-    List<Author> findAllByFirstName(String firstName);
-    Author findByEmail(String email);
-    List<Author> findAllByFirstNameIgnoreCase(String firstName);
-    List<Author> findAllByFirstNameContainingIgnoreCase(String firstName);
+    //=============================== JPA query methods ================================
+//    List<Author> findAllByFirstName(String firstName);
+//    Author findByEmail(String email);
+//    List<Author> findAllByFirstNameIgnoreCase(String firstName);
+//    List<Author> findAllByFirstNameContainingIgnoreCase(String firstName);
+
+    //=============================== JPQL ================================
+
+//    @Query("SELECT a FROM Author a WHERE a.firstName = :firstName")
+//    List<Author> findAllByFirstName(@Param("firstName") String firstName);
+//
+//    @Query("SELECT a FROM Author a WHERE a.email = :email")
+//    Author findByEmail(@Param("email") String email);
+//
+//    @Query("SELECT a FROM Author a WHERE LOWER(a.firstName) = LOWER(:firstName)")
+//    List<Author> findAllByFirstNameIgnoreCase(@Param("firstName") String firstName);
+//
+//    @Query("SELECT a FROM Author a WHERE LOWER(a.firstName) LIKE LOWER(CONCAT('%', :firstName, '%'))")
+//    List<Author> findAllByFirstNameContainingIgnoreCase(@Param("firstName") String firstName);
+
+    //=============================== Native SQL queries ================================
+
+    @Query(value = "SELECT * FROM Author a WHERE a.firstName = :firstName", nativeQuery = true)
+    List<Author> findAllByFirstName(@Param("firstName") String firstName);
+
+    @Query(value = "SELECT * FROM Author a WHERE a.email = :email", nativeQuery = true)
+    Author findByEmail(@Param("email") String email);
+
+    @Query(value = "SELECT * FROM Author a WHERE LOWER(a.firstName) = LOWER(:firstName)", nativeQuery = true)
+    List<Author> findAllByFirstNameIgnoreCase(@Param("firstName") String firstName);
+
+    @Query(value = "SELECT * FROM Author a WHERE LOWER(a.firstName) LIKE LOWER(CONCAT('%', :firstName, '%'))", nativeQuery = true)
+    List<Author> findAllByFirstNameContainingIgnoreCase(@Param("firstName") String firstName);
 }
